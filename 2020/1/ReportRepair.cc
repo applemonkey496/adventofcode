fd05a807959f5def60840325dcb6a52a4ad48423
applemonkey496 <55333787+applemonkey496@users.noreply.github.com>
2020-12-02 05:31:52 +0000

    day 1 solutions

---
+++ 2020/1/ReportRepair.cc
@@ -0,0 +1,61 @@
+
+// Input: https://adventofcode.com/2020/day/1/input
+
+#include <iostream>
+#include "input.hh"
+
+using std::cout;
+using std::endl;
+
+size_t inputSize = sizeof(input) / sizeof(int);
+
+void findTwo() {
+    // Loop through every combination of the numbers. This 
+    // isn't very efficient, but it doesn't need to be, 
+    // because it runs in under a second anyways.
+    for (int i = 0; i < inputSize; i++) {
+        for (int j = 0; j < inputSize; j++) {
+            int number1 = input[i];
+            int number2 = input[j];
+
+            // If the numbers sum to 2020, return their product
+            // Only if the two numbers are at different indices
+            if (number1 + number2 == 2020 && i != j) {
+                cout << number1 * number2 << endl;
+                return;
+            }
+        }
+    }
+
+    // Nothing found, return false and show message
+    cout << "No two numbers match." << endl;
+    return;
+}
+
+void findThree() {
+    // Again, this doesn't have to be efficient
+    // because it runs really fast anyways.
+    for (int i = 0; i < inputSize; i++) {
+        for (int j = 0; j < inputSize; j++) {
+            for (int k = 0; k < inputSize; k++) {
+                int number1 = input[i];
+                int number2 = input[j];
+                int number3 = input[k];
+
+                if (number1 + number2 + number3 == 2020 && i != j != k) {
+                    cout << number1 * number2 * number3 << endl;
+                    return;
+                }
+            }
+        }
+    }
+
+    // Nothing found, exit with error
+    cout << "No three numbers match." << endl;
+    return;
+}
+
+int main() {
+    findTwo();
+    findThree();
+}
